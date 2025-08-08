9689a745084a2eb0dcbe2e17721f829a4d0dc9b1
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-08-08 11:14:24 +0200

    feat: snack 5 done

--- script.js
+++ script.js
@@ -133,7 +133,27 @@ console.log(agesSum)
 console.log(media)
 
 
+// Snack 5
 
+
+// Usando la l'API https://boolean-spec-frontend.vercel.app/freetestapi/books/{id} usa la combinazione di .map() e Promise.all(), per creare una funzione (getBooks) che a partire da un array di id (ids), ritorna una promise che risolve un array di libri (books).
+// Testala con l’array [2, 13, 7, 21, 19] .
+
+
+async function fetchJson(url) {
+    const response = await fetch(url)
+    const obj = await response.json()
+    return obj
+}
+
+function getBooks(ids) {
+    const promises = ids.map(id => fetchJson(`http://localhost:3333/books/${id}`))
+    return Promise.all(promises)
+}
+
+getBooks([2, 13, 7, 21, 19])
+.then(books => console.log(books))
+.catch(error => console.error(error))
 
 
 
